ea4f0e7c79b52b0c59284fce13b735a40096461d
Edy <eduard@baun.de>
2012-03-05 14:00:07 +0100

    addUserToPad

--- node/handler/AuthHandler.js
+++ node/handler/AuthHandler.js
@@ -197,3 +197,36 @@ everyauth.hasPadAccess = function (req, res, next) {
 		});
 	});	
 };
+
+// adds an user to the pads access list
+padManager.addUserToPad = function (userID, padID, callback) {
+	padManager.doesPadExists(padID, function(err, padExists) {
+		if (!padExists) {
+			callback && callback(null);
+			return;
+		}
+
+		db.get('padaccess:'+padID, function(err, padAccess) {
+			// create a new access object
+			if (!padAccess) {
+				padAccess = {
+					owner: userID,
+					user: [userID],
+					reviewer: []
+				};
+
+			// alter available pad access object
+			} else if (padAccess.user.indexOf(userID) === -1) {
+				padAccess.user.push(userID);
+
+			// user has access
+			} else {
+				console.log('user has already access to pad ', padID);
+			}
+
+			db.set('padaccess:'+padID, padAccess);
+
+			callback && callback(null);
+		});
+	});
+};
