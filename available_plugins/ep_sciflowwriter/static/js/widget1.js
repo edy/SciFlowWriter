212825bc46d7df8c2692b1664945c74833e2f7c9
Edy <eduard@baun.de>
2012-04-12 00:43:22 +0200

    load widget bar dynamically

--- available_plugins/ep_sciflowwriter/static/js/widget1.js
+++ available_plugins/ep_sciflowwriter/static/js/widget1.js
@@ -1,5 +1,6 @@
 var $ = require('ep_etherpad-lite/static/js/rjquery').$; // use jQuery
 
 exports.postAceInit = function (hook_name, args, cb) {
-  $('body').prepend('<strong>huhu</strong>');
+  $('<div id="widgetcontainerbox"><div class="widget-column"></div><span class="vertical-rule-main"></span><span class="vertical-rule-corner-top"></span><span class="vertical-rule-corner-bottom"></span></div>').insertAfter('#editorcontainerbox');
+  $('<link rel="stylesheet" type="text/css" href="/static/plugins/ep_sciflowwriter/static/css/widgets.css">').appendTo("head");
 }
