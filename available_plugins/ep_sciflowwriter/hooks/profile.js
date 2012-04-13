eef34904f4886f71610a6d2b31db1c27e36924d6
Edy <eduard@baun.de>
2012-04-13 19:24:58 +0200

    use ejs in profile.html

--- available_plugins/ep_sciflowwriter/hooks/profile.js
+++ available_plugins/ep_sciflowwriter/hooks/profile.js
@@ -7,7 +7,17 @@ exports.expressCreateServer = function (hook_name, args, cb) {
 	//serve profile.html under /
 	args.app.get('/', function(req, res)
 	{	
-		res.send(eejs.require("ep_sciflowwriter/templates/profile.html"), { maxAge: 0 });
+		var user = req.user || {};
+		var profile = {
+			id: user.id,
+			name: user.name,
+			email: user.email || '',
+			image: user.auth.image || '',
+			url: user.auth.url || '',
+			pads: user.pads
+		};
+
+		res.send(eejs.require("ep_sciflowwriter/templates/profile.html", {'user': profile}), { maxAge: 0 });
 	});
 
 	args.app.get('/profile/:action?', profileHandler.handler);
