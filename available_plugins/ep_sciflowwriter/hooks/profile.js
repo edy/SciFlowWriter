83b9a0159d6bd5cb69ab105acd0981ff845ee2ee
Edy <eduard@baun.de>
2012-04-13 17:59:07 +0200

    some little refactoring

--- available_plugins/ep_sciflowwriter/hooks/profile.js
+++ available_plugins/ep_sciflowwriter/hooks/profile.js
@@ -7,8 +7,7 @@ exports.expressCreateServer = function (hook_name, args, cb) {
 	//serve profile.html under /
 	args.app.get('/', function(req, res)
 	{	
-		var filePath = path.normalize(__dirname + "/../templates/profile.html");
-		res.send(eejs.require(filePath), { maxAge: 0 });
+		res.send(eejs.require("ep_sciflowwriter/templates/profile.html"), { maxAge: 0 });
 	});
 
 	args.app.get('/profile/:action?', profileHandler.handler);
