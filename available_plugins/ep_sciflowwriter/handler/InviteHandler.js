cc40d399ce250021f3454bb10a01bcbe7a045160
Edy <eduard@baun.de>
2012-04-29 12:49:46 +0200

    check if user is logged in

--- available_plugins/ep_sciflowwriter/handler/InviteHandler.js
+++ available_plugins/ep_sciflowwriter/handler/InviteHandler.js
@@ -4,15 +4,20 @@ var authorManager = require('ep_etherpad-lite/node/db/AuthorManager');
 var authHandler = require('../handler/AuthHandler');
 
 exports.handler = function (req, res, next) {
-	db.get("padinvite:" + req.params.id, function(err, invite){
-		if (!invite) {
-			res.send('invalid id', 500);
-			return;
-		}
+	if(authHandler.isLoggedIn(req)) {
+		db.get("padinvite:" + req.params.id, function(err, invite){
+	
+			if (!invite) {
+				res.send('invalid invite id', 500);
+				return;
+			}
 
-		padManager.addUserToPad(req.user.id, invite.pad, function(){
-			authorManager.addPad(req.user.id, invite.type, invite.pad, function(){
-				res.redirect('/p/'+invite.pad);
+			padManager.addUserToPad(req.user.id, invite.pad, function(){
+				authorManager.addPad(req.user.id, invite.type, invite.pad, function(){
+					// remove the invite id since the user has pad access now
+					db.remove("padinvite:" + req.params.id);
+					res.redirect('/p/'+invite.pad);
+				});
 			});
 		});
 	});
