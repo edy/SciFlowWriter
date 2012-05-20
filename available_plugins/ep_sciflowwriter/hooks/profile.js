a683bafe26d6cf8b04785ed5a69f977670753e92
Edy <eduard@baun.de>
2012-05-20 11:05:06 +0200

    profile refactoring
    title from metadata and author names are displayed

--- available_plugins/ep_sciflowwriter/hooks/profile.js
+++ available_plugins/ep_sciflowwriter/hooks/profile.js
@@ -1,8 +1,10 @@
 var path = require('path');
 var eejs = require('ep_etherpad-lite/node/eejs');
+var async = require('ep_etherpad-lite/node_modules/async');
 var profileHandler = require('../handler/ProfileHandler');
 var authHandler = require('../handler/AuthHandler');
 var authorManager = require('../db/AuthorManager');
+var padManager = require('../db/PadManager');
 
 exports.expressCreateServer = function (hook_name, args, cb) {
 
@@ -11,19 +13,73 @@ exports.expressCreateServer = function (hook_name, args, cb) {
 	{	
 		if (!authHandler.isLoggedIn(req)) {
 			res.send('accedd denied');
-		} else {
-			var user = req.user;
-			var profile = {
-				id: user.id,
-				name: user.name,
-				email: user.email,
-				image: user.auth.image,
-				url: user.auth.url,
-				pads: user.pads
-			};
-
-			res.send(eejs.require("ep_sciflowwriter/templates/profile.html", {'user': profile}), { maxAge: 0 });
+			return;
 		}
+
+		var user = req.user;
+		var profile = {
+			id: user.id,
+			name: user.name,
+			email: user.email,
+			image: user.auth.image,
+			url: user.auth.url,
+			pads: user.pads
+		};
+
+		async.parallel({
+			my: function(callback) {
+				getPadData(user.pads.my, function(err, pads) {
+					callback(err, pads);
+				});
+			},
+			other: function(callback) {
+				getPadData(user.pads.other, function(err, pads) {
+					callback(err, pads);
+				});
+			}
+		}, function(err, results) {
+			console.error(results);
+			profile.pads.my = results.my;
+			profile.pads.other = results.other;
+			res.send(eejs.require("ep_sciflowwriter/templates/profile.html", {'user': profile}), { maxAge: 0 });
+		});
+
+		function getPadData(pads, callback) {
+			var p = [];
+
+			async.forEach(pads, function(padID, callback){
+				padManager.getPad(padID, function(err, pad) {
+					var title = padID;
+					var metadata = pad.getData('metadata');
+					if (metadata) {
+						title = metadata.title || padID;
+					}
+					
+					var access = pad.getData('access');
+
+					getAuthorData(access.user, function(err, authors) {
+						p.push({padID: padID, title: title, headRevision: pad.head, authors: authors.join(', ')});
+						callback(null);
+					});
+				});
+				
+			}, function(err) {
+				callback(null, p);
+			});
+		}
+
+		function getAuthorData(authors, callback) {
+			var a = [];
+			async.forEach(authors, function(authorID, callback){
+				authorManager.getAuthor(authorID, function(err, author){
+					a.push(author.name);
+					callback(null);
+				});
+			}, function(err) {
+				callback(err, a);
+			});
+		}
+
 	});
 
 	args.app.get('/profile/:action?', profileHandler.handler);
@@ -43,3 +99,4 @@ exports.socketio = function (hook_name, args, cb) {
 		});
 	});
 };
+
